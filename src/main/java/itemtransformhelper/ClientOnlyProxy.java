0612b99c4f45bdb1963e2ee78b74cc3adab00aca
The Grey Ghost <spamblackhole88@yahoo.com.au>
2015-01-26 23:35:37 +1030

    Initial commit

---
+++ src/main/java/itemtransformhelper/ClientOnlyProxy.java
@@ -0,0 +1,38 @@
+package itemtransformhelper;
+
+/**
+ * ClientProxy is used to set up the mod and start it running on normal minecraft.  It contains all the code that should run on the
+ *   client side only.
+ *   For more background information see here http://greyminecraftcoder.blogspot.com/2013/11/how-forge-starts-up-your-code.html
+ */
+public class ClientOnlyProxy extends CommonProxy
+{
+
+  /**
+   * Run before anything else. Read your config, create blocks, items, etc, and register them with the GameRegistry
+   */
+  public void preInit()
+  {
+    super.preInit();
+    StartupClientOnly.preInitClientOnly();
+  }
+
+  /**
+   * Do your mod setup. Build whatever data structures you care about. Register recipes,
+   * send FMLInterModComms messages to other mods.
+   */
+  public void init()
+  {
+    super.init();
+    StartupClientOnly.initClientOnly();
+  }
+
+  /**
+   * Handle interaction with other mods, complete your setup based on this.
+   */
+  public void postInit()
+  {
+    super.postInit();
+    StartupClientOnly.postInitClientOnly();
+  }
+}
