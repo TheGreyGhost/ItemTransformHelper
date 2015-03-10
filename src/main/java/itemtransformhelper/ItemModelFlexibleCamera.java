eab5b8267fb683e649b95266ee64865513f8fa52
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2015-03-10 15:40:59 +0200

    Wrap and don't erase IFlexibleBakedModel and ISmartItemModel interfaces.

--- src/main/java/itemtransformhelper/ItemModelFlexibleCamera.java
+++ src/main/java/itemtransformhelper/ItemModelFlexibleCamera.java
@@ -5,8 +5,13 @@ import java.util.List;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.client.resources.model.IBakedModel;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
+import net.minecraftforge.client.model.Attributes;
+import net.minecraftforge.client.model.IFlexibleBakedModel;
+import net.minecraftforge.client.model.ISmartItemModel;
 
 /**
  * User: The Grey Ghost
@@ -21,12 +26,13 @@ import net.minecraft.util.EnumFacing;
  * Models which don't match itemModelToOverride will use their original transform
  */
 @SuppressWarnings({ "deprecation", "unchecked" })
-public class ItemModelFlexibleCamera implements IBakedModel
+public class ItemModelFlexibleCamera implements IFlexibleBakedModel, ISmartItemModel
 {
   public ItemModelFlexibleCamera(IBakedModel i_modelToWrap, UpdateLink linkToCurrentInformation)
   {
     updateLink = linkToCurrentInformation;
     iBakedModel = i_modelToWrap;
+    wrappedModel = i_modelToWrap;
   }
 
   @Override
@@ -64,18 +70,35 @@ public class ItemModelFlexibleCamera implements IBakedModel
     return (updateLink.itemModelToOverride == this) ? updateLink.forcedTransform : iBakedModel.getItemCameraTransforms();
   }
 
+  @Override
+  public IBakedModel handleItemState(ItemStack stack) {
+      if (wrappedModel instanceof ISmartItemModel) {
+          iBakedModel = ((ISmartItemModel)wrappedModel).handleItemState(stack);
+      }
+      return this;
+  }
+
+  @Override
+  public VertexFormat getFormat() {
+      if (iBakedModel instanceof IFlexibleBakedModel)
+      {
+          return ((IFlexibleBakedModel)iBakedModel).getFormat();
+      }
+      return Attributes.DEFAULT_BAKED_FORMAT;
+  }
+
   private final UpdateLink updateLink;
 
   public IBakedModel getIBakedModel() {
     return iBakedModel;
   }
 
-  private final IBakedModel iBakedModel;
+  private IBakedModel iBakedModel;
+  private IBakedModel wrappedModel;
 
   public static class UpdateLink
   {
     public IBakedModel itemModelToOverride;
     public ItemCameraTransforms forcedTransform;
   }
-
 }
