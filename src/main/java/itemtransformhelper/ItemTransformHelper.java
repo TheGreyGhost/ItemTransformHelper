965c558af3074cf40c9466c1369b83c63f2e59e6
The Grey Ghost <spamblackhole88@yahoo.com.au>
2015-05-25 23:14:48 +0930

    Changed version number

--- src/main/java/itemtransformhelper/ItemTransformHelper.java
+++ src/main/java/itemtransformhelper/ItemTransformHelper.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class ItemTransformHelper
 {
     public static final String MODID = "itemtransformhelper";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3";
 
     // The instance of your mod that Forge uses.  Optional.
     @Mod.Instance(ItemTransformHelper.MODID)
