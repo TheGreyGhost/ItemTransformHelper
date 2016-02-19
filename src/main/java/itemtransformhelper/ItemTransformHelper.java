5e9725a0f01c4a645704c303f9aa11927f5b5f84
TheGreyGhost <spamblackhole88@yahoo.com.au>
2016-02-19 23:14:53 +1030

    Update to 1-8-9; added FIXED and GROUND.  Version number changed.

--- src/main/java/itemtransformhelper/ItemTransformHelper.java
+++ src/main/java/itemtransformhelper/ItemTransformHelper.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class ItemTransformHelper
 {
     public static final String MODID = "itemtransformhelper";
-    public static final String VERSION = "1.5";
+    public static final String VERSION = "1.6";
 
     // The instance of your mod that Forge uses.  Optional.
     @Mod.Instance(ItemTransformHelper.MODID)
