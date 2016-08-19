f6e12bde14b647b071f6d0991cc5edc288df6fb7
The Grey Ghost <spamblackhole88@yahoo.com.au>
2016-08-19 14:23:26 +0930

    Backport to 1.9.4 - no code changes needed at all!

--- src/main/java/itemtransformhelper/ItemTransformHelper.java
+++ src/main/java/itemtransformhelper/ItemTransformHelper.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class ItemTransformHelper
 {
     public static final String MODID = "itemtransformhelper";
-    public static final String VERSION = "1.10.2b";
+    public static final String VERSION = "1.9.4a";
 
     // The instance of your mod that Forge uses.  Optional.
     @Mod.Instance(ItemTransformHelper.MODID)
